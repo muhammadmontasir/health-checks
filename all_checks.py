94986fdb994dd92f20fcb22a038de7c310628f61
Montasir <m.montasir97@gmail.com>
2020-07-09 14:05:58 +0600

    Iterate over a list of checks and messages to avoid code duplication

--- all_checks.py
+++ all_checks.py
@@ -15,7 +15,7 @@ def check_disk_full(disk, min_gb, min_percent):
     percent_free = 100 * du.free / du.total
     #calculate how many free gigabytes
     gigabytes_free = du.free / 2**30
-    if percent_free < min_gb or percent_free < min_percent:
+    if gigabytes_free < min_gb or percent_free < min_percent:
         return True
     return False 
 
@@ -26,12 +26,21 @@ def check_root_full():
 
 
 def main():
-    if check_reboot():
+    checks = [
+        (check_reboot, "Pending Reboot"),
+        (check_root_full, "Root partition full")
+    ]
+    for check, msg in checks:
+        if check():
+            print(msg)
+            sys.exit(1)
+
+    """if check_reboot():
         print("Pending Reboot.")
         sys.exit(1)
     if check_root_full():
         print("Root partition full.")
-        sys.exit(1)
+        sys.exit(1)"""
 
     print("Everything ok.")
     print("file updated.")
